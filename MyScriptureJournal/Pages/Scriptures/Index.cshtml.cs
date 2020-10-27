f85bbc443116bf970ee1a8dbd7c96626ff2f8476
robertsdana <dmr4of8@gmail.com>
2020-10-26 18:56:19 -0600

    Adding MyScriptureJournal

---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Data;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Data.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Data.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+
+        public IList<Scripture> Scripture { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Books { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SelectedBook { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            IQueryable<string> bookQuery = from s in _context.Scripture
+                                            orderby s.Book
+                                            select s.Book;
+            var scriptures = from s in _context.Scripture
+                         select s;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(SelectedBook))
+            {
+                scriptures = scriptures.Where(x => x.Book == SelectedBook);
+            }
+
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    scriptures = scriptures.OrderBy(s => s.DateAdded);
+                    break;
+                case "date_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.DateAdded);
+                    break;
+                default:
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+            }
+
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
