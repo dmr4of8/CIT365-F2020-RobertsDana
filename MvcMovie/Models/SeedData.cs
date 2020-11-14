b87bb682918d8d9721ba002e05026174dba3042c
robertsdana <dmr4of8@gmail.com>
2020-11-14 00:13:51 -0700

    MvcMovie

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Singles Ward",
+                        ReleaseDate = DateTime.Parse("2002-2-1"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The RM",
+                        ReleaseDate = DateTime.Parse("2003-1-31"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2003-2-23"),
+                        Genre = "Comedy Drama",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Baptists at Our Barbecue",
+                        ReleaseDate = DateTime.Parse("2004-10-8"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
